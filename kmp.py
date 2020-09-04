da018f8ec948a51812ab7ba3b6b77860d72cce10
mivankin <70775415+mivankin@users.noreply.github.com>
2020-09-04 22:12:58 +0300

    Add files via upload

---
+++ kmp.py
@@ -0,0 +1,40 @@
+def pi_function(a):
+	p = [0]*len(a)
+	i = 1
+	j = 0
+	while i < len(a):
+		if a[i] == a[j]:
+			p[i] = j+1
+			i += 1
+			j += 1
+		elif j == 0:
+			p[i] = 0
+			i += 1
+		else:
+			j = p[j-1]
+
+	return p
+
+def str_find(t, a, p):
+	k = 0
+	l = 0
+	while k < len(t):
+		if t[k] == a[l]:
+			k += 1
+			l += 1
+			if l == len(a):
+				return True
+		elif l == 0:
+			k += 1
+			if k == len(t):
+				return False
+		else:
+			l = p[l-1]
+	return False
+a = 'abcabcd'
+t = 'fghgfdfghghabcabjsdjabcabcfjsdjasabcabfghfgherewrabcabcfghgfdfghghabcabjsdjabcabcfjsdjasabcabfghfgherewrabcabc'
+out = pi_function(a)
+flag = str_find(t,a,out)
+print(flag)
+
+
